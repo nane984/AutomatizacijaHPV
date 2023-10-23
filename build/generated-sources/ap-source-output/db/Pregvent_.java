bf05288adc1ad3dd84909c5b367268bd352b0725
branko <br.scekic@gmail.com>
2023-10-23 16:45:26 +0200

    Kumulativan protok rucno racunanje

--- build/generated-sources/ap-source-output/db/Pregvent_.java
+++ build/generated-sources/ap-source-output/db/Pregvent_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-10-22T19:12:23")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-10-23T16:43:09")
 @StaticMetamodel(Pregvent.class)
 public class Pregvent_ { 
 
